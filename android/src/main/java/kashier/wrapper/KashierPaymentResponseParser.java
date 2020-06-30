3f3c0cdf77d59b3abb6c3d84de2c8a9b7c5410ee
Meena Bassem <mbassem@elements-ft.com>
2020-06-30 18:54:27 +0200

    - Added parsing for PaymentResponse.ts model

--- android/src/main/java/kashier/wrapper/KashierPaymentResponseParser.java
+++ android/src/main/java/kashier/wrapper/KashierPaymentResponseParser.java
@@ -7,6 +7,9 @@ import io.kashier.sdk.Core.Helpers.ResponseHelper.ResponseHelper;
 import io.kashier.sdk.Core.model.Response.I_GenericResponse.I_GenericResponse;
 import io.kashier.sdk.Core.model.Response.I_GenericResponse.I_Messages;
 import io.kashier.sdk.Core.model.Response.Payment.Card;
+import io.kashier.sdk.Core.model.Response.Payment.CardInfo;
+import io.kashier.sdk.Core.model.Response.Payment.DSecure;
+import io.kashier.sdk.Core.model.Response.Payment.Merchant;
 import io.kashier.sdk.Core.model.Response.Payment.Messages;
 import io.kashier.sdk.Core.model.Response.Payment.PaymentResponse;
 import retrofit2.Response;
@@ -101,15 +104,73 @@ public class KashierPaymentResponseParser {
                     WritableMap _responseCardMap = new WritableNativeMap();
 
 
+                    if (_responseCard.getResult() != null) {
+                        _responseCardMap.putString("result", _responseCard.getResult());
+                    }
+                    if (_responseCard.getCurrency() != null) {
+                        _responseCardMap.putString("currency", _responseCard.getCurrency());
+                    }
+                    _responseCardMap.putDouble("amount", _responseCard.getAmount());
+                    if (_responseCard.getCardInfo() != null) {
+                        WritableMap _cardInfoMap = new WritableNativeMap();
+                        CardInfo _cardInfo = _responseCard.getCardInfo();
+                        if (_cardInfo.getCardDataToken() != null) {
+                            _cardInfoMap.putString("cardDataToken", _cardInfo.getCardDataToken());
+                        }
+                        if (_cardInfo.getCardHash() != null) {
+                            _cardInfoMap.putString("cardHash", _cardInfo.getCardHash());
+                        }
+                        if (_cardInfo.getMaskedCard() != null) {
+                            _cardInfoMap.putString("maskedCard", _cardInfo.getMaskedCard());
+                        }
+                        if (_cardInfo.getCardHolderName() != null) {
+                            _cardInfoMap.putString("cardHolderName", _cardInfo.getCardHolderName());
+                        }
+                        if (_cardInfo.getExpiryMonth() != null) {
+                            _cardInfoMap.putString("expiryMonth", _cardInfo.getExpiryMonth());
+                        }
+                        if (_cardInfo.getExpiryYear() != null) {
+                            _cardInfoMap.putString("expiryYear", _cardInfo.getExpiryYear());
+                        }
+                        if (_cardInfo.getCardBrand() != null) {
+                            _cardInfoMap.putString("cardBrand", _cardInfo.getCardBrand());
+                        }
+                        if (_cardInfo.getCcvToken() != null) {
+                            _cardInfoMap.putString("ccvToken", _cardInfo.getCcvToken());
+                        }
+                        _responseCardMap.putMap("cardInfo", _cardInfoMap);
+                    }
+                    if (_responseCard.getMerchant() != null) {
+                        WritableMap _merchantMap = new WritableNativeMap();
+                        if (_responseCard.getMerchant().getMerchantRedirectUrl() != null) {
+                            _merchantMap.putString("merchantRedirectUrl", _responseCard.getMerchant().getMerchantRedirectUrl());
+                        }
+                        _responseCardMap.putMap("merchant", _merchantMap);
+                    }
+                    if (_responseCard.getDSecure() != null) {
+                        WritableMap _3dSecureMap = new WritableNativeMap();
+                        DSecure _3DSecure = _responseCard.getDSecure();
+
+                        if (_3DSecure.getAcsUrl() != null) {
+                            _3dSecureMap.putString("acsUrl", _3DSecure.getAcsUrl());
+                        }
+                        if (_3DSecure.getProcessACSRedirectURL() != null) {
+                            _3dSecureMap.putString("processACSRedirectURL", _3DSecure.getProcessACSRedirectURL());
+                        }
+                        if (_3DSecure.getRequestType() != null) {
+                            _3dSecureMap.putString("requestType", _3DSecure.getRequestType());
+                        }
+                        if (_3DSecure.getPaReq() != null) {
+                            _3dSecureMap.putString("paReq", _3DSecure.getPaReq());
+                        }
+                        _responseCardMap.putMap("DSecure", _3dSecureMap);
+                    }
 
                     _payment_response.putMap("card", _responseCardMap);
-
                 }
 
                 _paymentResponseMap.putMap("response", _payment_response);
             }
-
-
         }
         return _paymentResponseMap;
     }
--- src/Services/Payment/PaymentResponse.ts
+++ src/Services/Payment/PaymentResponse.ts
@@ -8,8 +8,48 @@ interface Messages {
   ar: string;
   en: string;
 }
-interface _response{
 
+interface CardInfo {
+  cardDataToken: string;
+  cardHash: string;
+  maskedCard: string;
+  cardHolderName: string;
+  expiryMonth: string;
+  expiryYear: string;
+  cardBrand: string;
+  ccvToken: string;
+}
+interface Merchant {
+  merchantRedirectUrl: string;
+}
+interface DSecure {
+  acsUrl: string;
+  processACSRedirectURL: string;
+  requestType: string;
+  paReq: string;
+}
+interface card {
+  result: string;
+  currency: string;
+  amount: number;
+  cardInfo: CardInfo;
+  merchant: Merchant;
+  DSecure: DSecure;
+}
+interface _response {
+  method: string;
+  orderId: string;
+  apiKeyId: string;
+  merchantOrderId: string;
+  creationDate: string;
+  transactionId: string;
+  providerType: string;
+  merchantId: string;
+  orderReference: string;
+  refId: string;
+  status: string;
+  error: string;
+  card: card;
 }
 interface PaymentResponse {
   response: _response;
